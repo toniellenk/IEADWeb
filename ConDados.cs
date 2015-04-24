bc6455ef1878cea36a1129f44f8487dcaa1e7acd
toniellenk <toniel_lm@hotmail.com>
2015-04-24 13:39:56 -0400

    Update ConDados.cs

--- ConDados.cs
+++ ConDados.cs
@@ -12,11 +12,12 @@ namespace IEADWebApp
     {
 
         string SrtCon = ConfigurationManager.ConnectionStrings["root"].ConnectionString;
-        string StrSelect = "select * from t0050";
         protected DataSet ds = new DataSet();
         protected DataTable dt = new DataTable();
 
         public DataTable Consulta() { 
+            
+                private string StrSelect = "select * from t0050";
         
                 /*Cria o objeto de conexão com o banco */
                 SqlConnection ObjConn = new SqlConnection(SrtCon);
@@ -26,6 +27,25 @@ namespace IEADWebApp
                 /*Executa o comando*/
                 SqlDataAdapter da = new SqlDataAdapter(ObjCmd);
                 /*Abre a conexão */
+                da.Fill(ds);
+                dt = ds.Tables[0];
+                ObjConn.Close();
+                return dt;
+        }
+        
+                public DataTable Consulta(int idmembro) { 
+                    
+                /*Query SQL*/    
+                private string StrSelect = "select * from t0050 where idmembro = @idmembro";
+               
+                SqlConnection ObjConn = new SqlConnection(SrtCon);
+                SqlCommand ObjCmd = new SqlCommand(StrSelect, ObjConn);
+                
+                /*Definição de parãmetros da Query */
+                ObjCmd.Parameters.AddWithValue("@idmembro", idmembro)
+                
+                ObjConn.Open();   
+                SqlDataAdapter da = new SqlDataAdapter(ObjCmd);
                 da.Fill(ds);
                 dt = ds.Tables[0];
                 ObjConn.Close();
