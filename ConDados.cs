9dba97c2261b7143c0435e5552b5ac01f1720183
toniellenk <toniel_lm@hotmail.com>
2015-04-28 14:00:48 -0400

    Elaboração do Insert

--- ConDados.cs
+++ ConDados.cs
@@ -12,16 +12,16 @@ namespace IEADWebApp
     {
 
         string SrtCon = ConfigurationManager.ConnectionStrings["root"].ConnectionString;
-        string StrSelect = "select * from t0050";
         protected DataSet ds = new DataSet();
         protected DataTable dt = new DataTable();
 
         public DataTable Consulta() { 
-        
+            
+                string SqlSelect = "select * from t0050";
                 /*Cria o objeto de conexão com o banco */
                 SqlConnection ObjConn = new SqlConnection(SrtCon);
                 /*Cria o objeto de execução do comando */
-                SqlCommand ObjCmd = new SqlCommand(StrSelect, ObjConn);
+                SqlCommand ObjCmd = new SqlCommand(SqlSelect, ObjConn);
                 ObjConn.Open();   
                 /*Executa o comando*/
                 SqlDataAdapter da = new SqlDataAdapter(ObjCmd);
@@ -36,10 +36,10 @@ namespace IEADWebApp
                 public DataTable Consulta(int idmembro) { 
                     
                 /*Query SQL*/    
-                private string StrSelect = "select * from t0050 where idmembro = @idmembro";
+                private string SqlSelect = "select * from t0050 where idmembro = @idmembro";
                
                 SqlConnection ObjConn = new SqlConnection(SrtCon);
-                SqlCommand ObjCmd = new SqlCommand(StrSelect, ObjConn);
+                SqlCommand ObjCmd = new SqlCommand(SqlSelect, ObjConn);
                 
                 /*Definição de parãmetros da Query */
                 ObjCmd.Parameters.AddWithValue("@idmembro", idmembro)
@@ -51,7 +51,29 @@ namespace IEADWebApp
                 ObjConn.Close();
                 return dt;
         }
-
+        
+        public void inserir(string nome,string sexo,string tel,string end,string uf,string cid,string cep,string tip)
+            {
+                private SqlInsert = "insert into t0050(nome, sexo, tel, end, uf, cid, cep, tip) values(@nome, @sexo, @tel, @end, @uf, @cid, @cep, @tip)";
+                SqlConnection ObjConn = new SqlConnection(SrtCon);
+                SqlCommand ObjCmd = new SqlCmd(ObjConn, SqlInsert);
+                
+                ObjCmd.Parameters.AddWithValue("@nome",nome);
+                ObjCmd.Parameters.AddWithValue("@sexo",sexo);
+                ObjCmd.Parameters.AddWithValue("@tel",tel);
+                ObjCmd.Parameters.AddWithValue("@end",tel);
+                ObjCmd.Parameters.AddWithValue("@uf",uf);
+                ObjCmd.Parameters.AddWithValue("@cid",cid);
+                ObjCmd.Parameters.AddWithValue("@cep",cep);
+                ObjCmd.Parameters.AddWithValue("@tip",tip);
+                
+                ObjConn.Open();
+                
+                ObjCmd.ExecuteNonQuery;
+                
+                ObjConn.Close();
+            }
+        
         public string nome {
             get { return SrtCon; }
         }
